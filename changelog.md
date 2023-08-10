35e47617745de7d3eef229cb3d595dd0d8904582
Eli <eli@hanokh.com.br>
2023-08-10 20:49:18 -0300

    Alterado o nome da pasta de configuração, ajustado versão no changelog e também ajustado o deploy e versão no pipeline.

--- changelog.md
+++ changelog.md
@@ -1,19 +1,25 @@
 ### Changelog
 ### Committer
 
+#### [Released]
+#### [0.3.0] - 2023-10-10
+##### Changed
+- Alterando nome de pasta de configuração.
+- Aplicado algumas melhorias no código.
+
 #### [Released] 
 #### [0.2.0]  - 2022-08-18
 ##### Added
-- Adicionado parametro remote para que possa ser selecionado
-  também o repositório remote que será sincronizado
+- Adicionado parametro remote para que possa ser selecionado.
+  também o repositório remote que será sincronizado.
 
 #### [Released]
 #### [0.1.1] - 2022-05-18
 ##### Fixed
-- Corrigido erro no uso de variável antes da atribuição
+- Corrigido erro no uso de variável antes da atribuição.
 
 #### [Released]
 #### [0.1.0-0] - 2022-05-10
 ##### Added
-- Acessa as pastas baixa atualizações e envia as alterações
+- Acessa as pastas baixa atualizações e envia as alterações.
   que aconteceram na pasta.
--- main.py
+++ main.py
@@ -11,23 +11,25 @@ path = ""
 
 def main():
     config = configparser.ConfigParser()
-    config_file = config_dir + "/etc/committer/committer.conf"
+    config_file = config_dir + "/conf/committer.conf"
     config.read(config_file)
     sections = config.sections()
 
-    if len(sections) == 0:
-        print("Não Há seções configuradas")
+    for section in sections:
+        title = config.get(section, 'title')
+        path = config.get(section, 'path')
+        remote = config.get(section, 'remote')
+        branch = config.get(section, 'branch')
+        os.chdir(path)
+        os.system("git pull {0} {1}".format(remote, branch))
+        os.system("git add .")
+        os.system("git commit -m '{0}'".format(title))
+        os.system("git push {0} {1}".format(remote, branch))
     else:
-        for section in sections:
-            title = config.get(section, 'title')
-            path = config.get(section, 'path')
-            remote = config.get(section, 'remote')
-            branch = config.get(section, 'branch')
-            os.chdir(path)
-            os.system("git pull {0} {1}".format(remote, branch))
-            os.system("git add .")
-            os.system("git commit -m '{0}'".format(title))
-            os.system("git push {0} {1}".format(remote, branch))
+        if len(sections) == 0:
+            print("Não há seções configuradas!")
+        else:
+            print("Fim da sincronização!")
 
 
 if __name__ == "__main__":
