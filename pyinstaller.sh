fc37f47686d128fba28f9f8fc94bdc4457015650
Eli Florêncio Pereira <elipe.pereira@gmail.com>
2023-10-30 16:25:54 -0300

    Adicionado funções ao installer para empacotamento e build

--- pyinstaller.sh
+++ pyinstaller.sh
@@ -1,19 +1,112 @@
 #!/bin/bash
 
-base_dir=`pwd`
-dist_path="."
-build_path="/tmp/build"
-work_path="${build_path}"
-spec_path="${build_path}"
-name="committer"
+function build(){
+    basedir=$(pwd)
+    build_path="/tmp/build"
+    dist_path="${build_path}/dist"
+    work_path="${build_path}"
+    spec_path="${build_path}"
+    name="committer"
 
-mkdir -p $spec_path
+    mkdir -p ${dist_path}
 
-pyinstaller --distpath $dist_path \
-        --add-data "$base_dir/conf:conf" \
-        --add-data "$base_dir/committer.cron:." \
-        --workpath $work_path \
-        --specpath $spec_path \
-        --name $name main.py
+    pyinstaller --distpath ${dist_path} \
+        --add-data "${basedir}/conf:conf" \
+        --add-data "${basedir}/committer.cron:." \
+        --workpath "${work_path}" \
+        --specpath "${spec_path}" \
+        --name "${name}" main.py
+}
 
-test -d $build_path && rm -rf $build_path
+function pack(){
+    appname=committer
+    package=committer
+    section=Misc
+    priority=optional
+    architecture=amd64
+    maintainer="Eli Florêncio Pereira"
+    depends="git"
+    description="Acessa pastas configuradas e faz commit das mudanças automaticamente"
+    version=$(cat version)
+    cmd_preinst="#!/bin/bash
+    ! test -f /etc/${appname}/${appname}.conf || cp -av /etc/${appname}/${appname}.conf /tmp
+    "
+    cmd_postinst="#!/bin/bash
+    test -h /etc/${appname} || ln -s /usr/share/${appname}/conf /etc/${appname}
+    test -h /usr/bin/${appname} || ln -s /usr/share/${appname}/${appname} /usr/bin/${appname}
+    test ! -f /tmp/${appname}.conf || cp -av /tmp/${appname}.conf /etc/${appname}/${appname}.conf
+    cp /usr/share/${appname}/${appname}.cron /etc/cron.d/${appname}
+    chmod 0644 /etc/cron.d/${appname}
+    "
+    cmd_postrm="#!/bin/bash
+    ! test -h /etc/hor || unlink /etc/hor
+    ! test -h /usr/bin/hor || unlink /usr/bin/hor
+    "
+
+    # Apaga builds antigos
+    test ! -d /tmp/${package}/DEBIAN || rm -rf /tmp/${package}
+
+    mkdir /tmp/${package}
+    mkdir /tmp/${package}/DEBIAN
+
+    {
+        echo Section:"${section}";
+        echo Package:"${package}";
+        echo Priority:"${priority}";
+        echo Version:"${version}";
+        echo Architecture:"${architecture}";
+        echo Maintainer:"${maintainer}";
+        echo Depends:"${depends}";
+        echo Description:"${description}";
+    } > /tmp/${package}/DEBIAN/control
+
+    echo "${cmd_preinst}" >> /tmp/${package}/DEBIAN/preinst
+    echo "${cmd_postinst}" >> /tmp/${package}/DEBIAN/postinst
+    echo "${cmd_postrm}" >> /tmp/${package}/DEBIAN/postrm
+
+    chmod +x /tmp/${package}/DEBIAN/*
+    mkdir -p /tmp/${package}/usr/share
+
+    build
+
+    cp -a /tmp/build/dist/${package} /tmp/${package}/usr/share
+
+    dpkg-deb -Zxz -b /tmp/$package .
+    test -d /tmp/${package}/DEBIAN && rm -rf /tmp/$package*
+}
+
+function clear(){
+    build_path=/tmp/build
+    package_name="committer"
+    package_build_path=/tmp/${package_name}
+
+    test -d ${build_path} && rm -rf ${build_path}
+    test -d ${package_build_path} && rm -rf ${package_build_path}
+}
+
+function help(){
+    echo "
+    help - Imprime essa mensagem de ajuda
+    pack - Compila o programa e empacota gerando na pasta raiz um arquivo .deb. 
+    build - Compila o programa, seguindo os parâmetros já estabelecidos na função.
+    clear - Apaga a pasta de build. Onde ficam os arquivos gerados pelo build e pack.
+    "
+}
+
+case "$1" in 
+    build)
+        build
+    ;;
+    pack)
+        pack
+    ;;
+    clear)
+        clear
+    ;;
+    help)
+        help
+    ;;
+    *)
+        help
+    ;;
+esac
